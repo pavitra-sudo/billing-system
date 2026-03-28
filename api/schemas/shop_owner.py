0e29f29947794074b2bac41db5d8a8c61061de10
pavitra-sudo <213853274+pavitra-sudo@users.noreply.github.com>
2026-03-28 19:58:15 +0530

    add shop_owner.py schema definitions for request and response

---
+++ api/schemas/shop_owner.py
@@ -0,0 +1,21 @@
+# schemas/shop_owner.py
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+# Request Schema (input)
+class ShopOwnerCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str = Field(..., min_length=8, max_length=255)
+
+
+#  Response Schema (output)
+class ShopOwnerResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    schema_name: str | None
+
+    class Config:
+        from_attributes = True  
