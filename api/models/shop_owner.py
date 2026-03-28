111de61f872be32660c7adb1cea6d392d44cc4df
pavitra-sudo <213853274+pavitra-sudo@users.noreply.github.com>
2026-03-28 19:58:36 +0530

    add shop_owner model with fields for id, name, email, password, and schema_name

---
+++ api/models/shop_owner.py
@@ -0,0 +1,14 @@
+# models/shop_owner.py
+
+from sqlalchemy import Column, Integer, String
+from ..database.db import PublicBase
+
+class ShopOwner(PublicBase):
+    __tablename__ = "shop_owner"
+    __table_args__ = {"schema": "public"}  # IMPORTANT
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(150), unique=True, nullable=False, index=True)
+    password = Column(String(255), nullable=False)
+    schema_name = Column(String(100), nullable=True)
