13b5671c39d976d25dfdeaa365c8d56b585571d9
pavitra-sudo <213853274+pavitra-sudo@users.noreply.github.com>
2026-03-30 10:57:41 +0530

    refactor: update import paths for tenant schemas and services

--- api/routers/tenant/createTenant.py
+++ api/routers/tenant/createTenant.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from ...database.db import get_db
-from ...schemas.tenant.tenantCreateSchema import TenantCreate, TenantResponse
-from ...services.tenant.tenantCreateService import ShopOwnerService
+from ...schemas.tenant.tenant import TenantCreate, TenantResponse
+from ...services.tenant.tenant import ShopOwnerService
 
 router = APIRouter(
     prefix="/api/auth/register",
